5d8148d2706d6f273f3e45ce0faa24a84cd77654
unknown <ramesh.dussa@zedexinfo.com>
2024-08-22 19:28:35 -0700

    practice

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -59,13 +59,14 @@ const Modal = ({ triggerBtn,open,setOpen ,details,setDetails}: ModalProps) => {
   }
 
   return (
-    <Dialog.Root open={open} onOpenChange={()=>{setOpen(!open)}} >
+    <Dialog.Root open={open} onOpenChange={()=>{setOpen(!open)} }
+    >
       <Dialog.Trigger asChild >
         {triggerBtn}
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] h-[auto] w-[50%] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-9 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Content onPointerDownOutside={(e)=>e.preventDefault()} className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] h-[auto] w-[50%] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-9 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium flex ">
             Add New Contact
           </Dialog.Title>
